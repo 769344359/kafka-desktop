b6dc5c408e82698b927cb909266896aa01ca1959
dai <dai@localhost.localdomain>
2024-06-23 11:48:45 -0400

    git@github.com:769344359/kafka-desktop.git

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+function App() {
+  // const [greetMsg, setGreetMsg] = useState("");
+  // const [name, setName] = useState("");
+
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   // setGreetMsg(await invoke("greet", { name }));
+  //   setGreetMsg(await invoke("my_custom_command"));
+    
+  // }
+
+  // return (
+  //   <div className="container">
+  //     <h1>Welcome to Tauri!</h1>
+
+  //     <div className="row">
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://tauri.app" target="_blank">
+  //         <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+  //       </a>
+  //     </div>
+
+  //     <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+  //     <form
+  //       className="row"
+  //       onSubmit={(e) => {
+  //         e.preventDefault();
+  //         greet();
+  //       }}
+  //     >
+  //       <input
+  //         id="greet-input"
+  //         onChange={(e) => setName(e.currentTarget.value)}
+  //         placeholder="Enter a name..."
+  //       />
+  //       <button type="submit">Greet</button>
+  //     </form>
+
+  //     <p>{greetMsg}</p>
+  //   </div>
+  // );
+  return (
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+import {Input} from "@nextui-org/input";
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Input type="email" label="Email" />
+      <Input type="email" label="Email" placeholder="Enter your email" />
+    </div>
+    </DefaultLayout>
+  );
+}
