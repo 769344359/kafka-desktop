fea2258f94cf1a6f2075b949e71c9be37cf15b42
dai <dai@localhost.localdomain>
2024-09-16 04:30:44 -0400

    feat: add main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import {Button, ButtonGroup} from "@nextui-org/button";
+import {Textarea} from "@nextui-org/input";
+
 import {Input} from "@nextui-org/input";
 export default function IndexPage() {
   return (
@@ -14,6 +17,18 @@ export default function IndexPage() {
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Input type="input" label="bootstrap" />
       <Input type="input" label="topic"/>
+    </div>
+    <div>
+    <Textarea
+      label="Description"
+      placeholder="Enter your description"
+      className="max-w-xs"
+    />
+    </div>
+    <div>
+    <Button color="primary">
+      Send
+    </Button>
     </div>
     </DefaultLayout>
   );
