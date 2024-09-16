01796216847fe454c78bd2b0eb29e5f1232f62f4
dinosaur <thedinosaurmail@gmail.com>
2024-09-16 09:34:35 -0400

    init send

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,24 @@ import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 import {Button, ButtonGroup} from "@nextui-org/button";
 import {Textarea} from "@nextui-org/input";
-
+import  { useState } from 'react';
 import {Input} from "@nextui-org/input";
+import { invoke } from "@tauri-apps/api/core";
+
 export default function IndexPage() {
+  const [bootstrap, setBootstrap] = useState('');
+  const [topic, setTopic] = useState('');
+  const buttonSubmit = () =>{
+    console.log( "topic is" + topic);
+    console.log("bootstrap is" + bootstrap);
+    invoke("send_kafka",{server:bootstrap ,topic:topic,message:"hihi"})
+
+  }
   return (
     <DefaultLayout>
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Input type="input" label="bootstrap" />
-      <Input type="input" label="topic"/>
+      <Input type="input" label="bootstrap server" value={bootstrap} onValueChange={setBootstrap} />
+      <Input type="input" label="topic" value={topic} onValueChange={setTopic} />
     </div>
     <div>
     <Textarea
@@ -26,7 +36,7 @@ export default function IndexPage() {
     />
     </div>
     <div>
-    <Button color="primary">
+    <Button color="primary" onClick={buttonSubmit}>
       Send
     </Button>
     </div>
