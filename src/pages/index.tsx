7a68e99b475ba7289b9a153b4103a3be9171bbf9
dinosaur <thedinosaurmail@gmail.com>
2024-09-17 10:57:03 -0400

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,14 @@ import {Input} from "@nextui-org/input";
 import { invoke } from "@tauri-apps/api/core";
 import toast, { Toaster } from 'react-hot-toast';
 import {Tabs, Tab} from "@nextui-org/tabs";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell
+} from "@nextui-org/table";
 export default function IndexPage() {
   const [bootstrap, setBootstrap] = useState('');
   const [topic, setTopic] = useState('');
@@ -45,7 +53,35 @@ export default function IndexPage() {
               </div>
            </Tab>
            <Tab key="topic" title="Topic">
-
+           <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow key="1">
+          <TableCell>Tony Reichert</TableCell>
+          <TableCell>CEO</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="2">
+          <TableCell>Zoey Lang</TableCell>
+          <TableCell>Technical Lead</TableCell>
+          <TableCell>Paused</TableCell>
+        </TableRow>
+        <TableRow key="3">
+          <TableCell>Jane Fisher</TableCell>
+          <TableCell>Senior Developer</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="4">
+          <TableCell>William Howard</TableCell>
+          <TableCell>Community Manager</TableCell>
+          <TableCell>Vacation</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
            </Tab>
     </Tabs>
     </DefaultLayout>
