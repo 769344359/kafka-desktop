1e0ce0f35067a59da412a30f4d015a55e053c780
dinosaur <thedinosaurmail@gmail.com>
2024-09-17 10:31:55 -0400

    modify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,4 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 import {Button, ButtonGroup} from "@nextui-org/button";
 import {Textarea} from "@nextui-org/input";
@@ -13,11 +6,12 @@ import  { useState } from 'react';
 import {Input} from "@nextui-org/input";
 import { invoke } from "@tauri-apps/api/core";
 import toast, { Toaster } from 'react-hot-toast';
-
+import {Tabs, Tab} from "@nextui-org/tabs";
 export default function IndexPage() {
   const [bootstrap, setBootstrap] = useState('');
   const [topic, setTopic] = useState('');
   const [message,setMessage] = useState('');
+  const [isVertical , setIsVertical] = useState(true);
   const buttonSubmit = () =>{
     console.log( "topic is" + topic);
     console.log("bootstrap is" + bootstrap);
@@ -28,24 +22,32 @@ export default function IndexPage() {
 }
   const theStyle={"margin-top":"20px"}
   return (
+    
     <DefaultLayout>
-    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Input type="input" label="bootstrap server" value={bootstrap} onValueChange={setBootstrap} />
-      <Input type="input" label="topic" value={topic} onValueChange={setTopic} />
-    </div>
-    <div style={theStyle}>
-    <Textarea
-      label="Description"
-      placeholder="Enter your description"
-      // className="max-w-xs"
-    />
-    </div>
-    <Toaster />
-    <div style={theStyle}>
-    <Button color="primary" onClick={buttonSubmit}>
-      Send
-    </Button>
-    </div>
+       <Tabs aria-label="Options" isVertical={isVertical}>
+          <Tab key="send_message" title="Send">
+              <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                <Input type="input" label="bootstrap server" value={bootstrap} onValueChange={setBootstrap} />
+                <Input type="input" label="topic" value={topic} onValueChange={setTopic} />
+              </div>
+              <div style={theStyle}>
+              <Textarea
+                label="Description"
+                placeholder="Enter your description"
+                // className="max-w-xs"
+              />
+              </div>
+              <Toaster />
+              <div style={theStyle}>
+              <Button color="primary" onClick={buttonSubmit}>
+                Send
+              </Button>
+              </div>
+           </Tab>
+           <Tab key="topic" title="Topic">
+
+           </Tab>
+    </Tabs>
     </DefaultLayout>
   );
 }
