9855add49a83413353af42e6398fadd6f15ab437
dinosaur <thedinosaurmail@gmail.com>
2024-10-02 02:56:31 -0400

    feat: 修改参数

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use rdkafka::client;
 use rdkafka::config::ClientConfig;
 use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::error::KafkaError;
 use rdkafka::groups::GroupList;
 use rdkafka::util::Timeout;
 use std::time::Duration;
@@ -85,62 +86,19 @@ pub fn get_all_group(cfg :&mut Config) -> Result<&mut Config,String>{
     println!("all group is {:?}" , cfg.groups);
         Ok(cfg)
 }
-pub fn get_all_topic(cfg :&mut Config) -> Result<&mut Config, String>{
+pub fn get_all_topic(cfg :&mut Config) -> Result<&mut Config,   KafkaError>{
 
     let consumer: BaseConsumer = ClientConfig::new()
     .set("bootstrap.servers", cfg.brokers.clone())
     .create()
     .expect("Consumer creation failed");
 let metadata = consumer
-.fetch_metadata(None,  Duration::new(5, 0))
-.expect("Failed to fetch metadata");
+.fetch_metadata(None,  Duration::new(5, 0))?;
 for topic in metadata.topics() {
     println!("  Topic: {}  Err: {:?}", topic.name(), topic.error());
     cfg.topics.push(String::from(topic.name()))
-    // for partition in topic.partitions() {
-    //     println!(
-    //         "     Partition: {}  Leader: {}  Replicas: {:?}  ISR: {:?}  Err: {:?}",
-    //         partition.id(),
-    //         partition.leader(),
-    //         partition.replicas(),
-    //         partition.isr(),
-    //         partition.error()
-    //     );
-    //     if fetch_offsets {
-    //         let (low, high) = consumer
-    //             .fetch_watermarks(topic.name(), partition.id(), Duration::from_secs(1))
-    //             .unwrap_or((-1, -1));
-    //         println!(
-    //             "       Low watermark: {}  High watermark: {} (difference: {})",
-    //             low,
-    //             high,
-    //             high - low
-    //         );
-    //         message_count += high - low;
-    //     }
-    // }
-    // if fetch_offsets {
-    //     println!("     Total message count: {}", message_count);
-    // }
 }
-    // let mut client = KafkaClient::new(cfg.brokers.clone());
-    // client.load_metadata_all().map_err(|e| e.to_string())?;
-    // // ~ determine the list of topics we're supposed to report about
-    // let topics = if cfg.topics.is_empty() {
-    //     let topics = client.topics();
-    //     let mut names = Vec::with_capacity(topics.len());
-    //     for topic in topics.names() {
-    //         names.push(topic.to_owned());
-    //     }
-    //     names.sort();
-    //     names
-    // } else {
-    //     cfg.topics.clone()
-    // };
-    
-    // // println!("topic------:{:?}", topics);
-    // // topics;
-    // cfg.topics = topics;
+
 
     Ok(cfg)
 }
